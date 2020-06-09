922231ff75e7d2e40008c1fd665a25bd4bf7c3bd
Lucky3028 <lucky3028.0@gmail.com>
2020-06-09 14:10:44 +0900

    [Add] 入力処理

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,22 @@
 use std::io;
-use std::collections::HashMap;
+use std::io::{stdin, stdout, Read, Write, BufRead};
+use std::collections::BTreeMap;
+use ansi_term::Colour::RGB;
 
 fn main() {
-    //変換する文字列を入力する処理
-    println!("変換したい文字列を入力してください。：");
-    //TODO: 1文字ずつor連続文
-    let target_str = {
-        let mut s = String::new();
-        io::stdin()
-            .read_line(&mut s)
-            .expect("文字列の読み込みに失敗しました。処理を終了します。");
-        s.trim_end().to_owned() //改行コード及び空白だけの場合それらを削除する
-    };
-    if  {target_str.is_empty()} {
-        println!("文字列が入力されていません。処理を終了します。");
-        return
-    }
-
-    let decorationcode = {
-        let mut m = HashMap::new();
-        m.insert("§k", "Obfuscated");
-        m.insert("§l", "Bold");
-        m.insert("§m", "Strikethrough");
-        m.insert("§n", "Underline");
-        m.insert("§o", "Italic");
-        m.insert("§r", "Reset");
+    let decoration_code = {
+        let mut m = BTreeMap::new();
+        m.insert("§l", ["Bold","太字"]);
+        m.insert("§o", ["Italic", "斜め"]);
+        m.insert("§n", ["Underline", "下線"]);
+        m.insert("§k", ["Obfuscated", "難読化"]);
+        m.insert("§m", ["Strike through", "取り消し線"]);
+        m.insert("§r", ["Reset", "文字リセット"]);
         m
     };
 
     let color_code = {
-        let mut m = HashMap::new();
+        let mut m = BTreeMap::new();
         m.insert("§9", "Blue");
         m.insert("§1", "Dark Blue");
         m.insert("§a", "Green");
@@ -49,4 +36,27 @@ fn main() {
         m
     };
 
+    println!("変換したい文字列を入力してください。：");
+    //TODO: 1文字ずつor連続文
+    let target_str = read_texts();
+    if  {target_str.is_empty()} {
+        println!("文字列が入力されていません。処理を終了します。");
+        return
+    }
+
+    println!("コードを入力してください。なお、装飾コード、カラーコードの順に入力してください。（例：bs02）");
+    println!("また、コード一覧を見たい場合はhelpと入力してください。");
+    let target_code = read_texts();
+    if  {target_code.is_empty()} {
+        println!("文字列が入力されていません。処理を終了します。");
+        return
+    }
+}
+
+fn read_texts() -> String {
+    let mut s = String::new();
+    io::stdin()
+        .read_line(&mut s)
+        .expect("文字列の読み込みに失敗しました。処理を終了します。");
+    s.trim_end().to_string()
 }
