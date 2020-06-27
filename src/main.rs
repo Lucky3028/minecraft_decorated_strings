09f2072c93a57683abbd60ab15e5c5edb2573dfd
Lucky3028 <lucky3028.0@gmail.com>
2020-06-27 22:19:17 +0900

    [Improve] エラー処理

--- src/main.rs
+++ src/main.rs
@@ -17,8 +17,7 @@ fn main() {
     println!("変換したい文字列を入力してください。/ Enter texts you want to decorate：");
     let target_str = read_texts();
     if target_str.is_empty() {
-        println!("文字列が入力されていません。/ No texts are entered.");
-        return;
+        exit_program("文字列が入力されていません。/ No texts are entered.");
     }
 
     println!("次のコードが利用できます。/ The codes below are available.");
@@ -40,8 +39,7 @@ fn main() {
     let target_color_code = read_texts().to_lowercase();
 
     if target_format_code.is_empty() && target_color_code.is_empty() {
-        println!("どちらのコードも入力されませんでした。/ Neither codes are entered.");
-        return;
+        exit_program("どちらのコードも入力されませんでした。/ Neither codes are entered.");
     }
 
     let mut found_format_code = String::new();
@@ -60,8 +58,8 @@ fn main() {
             found_format_code = match compare_format_id_and_code(f.to_owned(), found_format_code) {
                 Ok(n) => n,
                 Err(err) => {
-                    println!("Error: {}", err);
-                    return;
+                    exit_program(err.as_str());
+                    "ERR".to_string()
                 }
             };
         }
@@ -70,15 +68,16 @@ fn main() {
     let mut found_color_code = String::new();
     if !target_color_code.is_empty() {
         if target_color_code.len() != 2 {
-            println!("カラーコードは1つのみ指定できます。/ Only 1 color code can be specified.");
-            return;
+            exit_program(
+                "カラーコードは1つのみ指定できます。/ Only 1 color code can be specified.",
+            );
         }
 
         found_color_code = match compare_color_id_and_code(target_color_code, found_color_code) {
             Ok(n) => n,
             Err(err) => {
-                println!("Error: {}", err);
-                return;
+                exit_program(err.as_str());
+                "ERR".to_string()
             }
         };
     }
